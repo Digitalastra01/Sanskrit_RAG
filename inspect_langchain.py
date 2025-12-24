25493bb8d8d552953d4ec6bbafc378deed1bb1a1
avthemlguy01 <avneesh.g@toadsters.com>
2025-12-24 11:59:57 +0530

    Remove unused diagnostic scripts

--- inspect_langchain.py
+++
@@ -1,8 +0,0 @@
-import langchain
-print(dir(langchain))
-try:
-    import langchain.chains
-    print("langchain.chains imported")
-    print(dir(langchain.chains))
-except ImportError as e:
-    print(f"Error importing langchain.chains: {e}")
